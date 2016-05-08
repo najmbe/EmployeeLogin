e80020b6c6badcf7c2e1f6f1f7b73104aba209e5
najmbe <mail.najiyabegum@gmail.com>
2016-05-08 19:04:41 +0530

    Added unit tests

---
+++ WPFApps/EmployeeLogin/EmployeeLoginTests1/EmployeeViewModelTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmployeeLogin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace EmployeeLogin.Tests
+{
+    /// <summary>
+        /// Summary description for EmployeeViewModelTest
+        /// </summary>
+        [TestClass]
+        public class EmployeeViewModelTest
+        {
+            EmployeeViewModel employeeViewModel;
+
+            public EmployeeViewModelTest()
+            {
+                //arrange
+                employeeViewModel = new EmployeeViewModel();
+            }
+
+            [TestMethod]
+            public void CheckEmployeesNotNull()
+            {
+                //assert
+                CollectionAssert.AllItemsAreNotNull(employeeViewModel.Employees);
+           
+            }
+
+        [TestMethod]
+        public void SaveTest()
+        {
+            //act
+            bool result = employeeViewModel.Save.CanExecute(null);
+
+            //assert
+            Assert.IsTrue(result);
+
+        }
+
+        [TestMethod]
+        public void EmployeesCountNotTrueTest()
+        {
+            //arrange
+            var employees = new ObservableCollection<Employee>();
+            employees.Add(new Employee() { Id = 1, Address = "Blk 244/A", Email = "email1@gmail.com", FirstName = "FirstNameA", LastName = "LastNameA" });
+            employees.Add(new Employee() { Id = 2, Address = "Blk 244/B", Email = "email2@gmail.com", FirstName = "FirstNameB", LastName = "LastNameB" });
+
+            //assert
+            CollectionAssert.AreNotEqual(employees, employeeViewModel.Employees);
+
+        }
+    }
+    
+}
---
+++ WPFApps/EmployeeLogin/EmployeeLoginTests1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EmployeeLoginTests1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmployeeLoginTests1")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c07f00a7-1116-447f-848a-522371901e56")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
